b41337f8b4d83548545e31643ea4a4a35f46c0b4
Zane Reick <zane.reick@unknownts.tk>
2020-12-07 02:09:50 -0600

    Well, i need something to show

---
+++ ava-core.py
@@ -0,0 +1,62 @@
+import pyttsx3
+import speech_recognition as sr
+
+
+def speak(text):
+    """
+    The voice of AVA
+    """
+    # Initialize the engine
+    engine = pyttsx3.init(driverName=None, debug=True)
+
+    # Get and set female voice, because in this case Ava is a female.
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[1].id)
+
+    # Speak
+    engine.say(text)
+
+    # TODO Find and use a none blocking method so that commands can be cancelled
+    # Blocking method! Process all queued TTS commands
+    engine.runAndWait()
+
+def takeUserVoiceIn():
+    """
+    Get users voice and processes it
+    """
+    # Set up recognizer and source
+    recog = sr.Recognizer()
+    source = sr.Microphone()
+
+    recog.dynamic_energy_threshold = True
+
+    recog.pause_threshold = 0.8
+    audio = recog.listen(source) # Blocking?
+
+    # Start listening for data
+    print("Listening...")
+
+    try:
+        print("Proccessing...")
+
+        question = recog.recognize_wit(audio, "HZGX4AIGUWXQTIEOCP5ZGF6SVRNAKOBJ", False)
+
+    except Exception as e:
+        print(e)
+        return "None"
+    
+    return question
+
+
+
+def voice_commands():
+    """
+    Process commands
+    """
+    while True:
+        query = takeUserVoiceIn().lower()
+        print(query)
+
+if __name__ == '__main__':
+    # Run voice_commands
+    voice_commands()
