1fc5489968cc9f6035d6a92ca3343374206c8132
Zane Reick <zane.reick@unknownts.tk>
2020-12-12 22:42:06 -0600

    Cont'd work on the verbal trigger programmer

--- function_tests.py
+++ function_tests.py
@@ -1,4 +1,7 @@
-import ava_cmds
+#import ava_cmds
 #import ava_core
+import v_programmer
 
-ava_cmds.speak("Test")
+#ava_cmds.speak("Test")
+
+v_programmer.write_verbal_command()
--- v_programmer.py
+++ v_programmer.py
@@ -1,3 +1,29 @@
 """
 The verbal programmer module for AVA
 """
+
+import json
+
+# TODO Faster lookup. Maybe use a table of IDs? Distribute 
+
+async def write_verbal_command(t_phrase, orig_cmd): # Test, async should make this faster (AVA shouldnt have to wait for file write operations)
+    """
+    Asynchronous function to write new verbal commands to a DB 
+    """
+    data = {}
+    data['verbal'].append({
+        'id': None,
+        'trigger': str(t_phrase),
+        'original': str(orig_cmd)
+    })
+    with open('c_v_cmds_db.json', 'w') as output:
+        json.dump(data, output)
+
+def read_verbal_command(t_phrase): # Make async?
+    with open('c_v_cmds_db.json', 'r') as input:
+        data = json.load(input)
+        for trigger in data['trigger']:
+        if str(t_phrase) == str(trigger):
+            return data['original']
+        else:
+            return False
