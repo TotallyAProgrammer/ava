a768e589ce466c5be19cef0d85ca043db28dbdd4
Zane Reick <zane.reick@unknownts.tk>
2020-12-15 11:18:43 -0600

    Cont'd work on the cloud server

--- ava_cloud/ava_cloud_server.py
+++ ava_cloud/ava_cloud_server.py
@@ -1,7 +1,11 @@
+import sys
 import socket
 import threading
 
 server_version = 1
+
+# Server binding
+
 BIND_PORT = 25680
 BIND_ADDR = "0.0.0.0"
 
@@ -11,8 +15,28 @@ try:
     s.bind((BIND_ADDR, BIND_PORT))
 except socket.error as exp:
     print("Binding failed. Error: " + str(msg[0]) + " Message " + msg[1])
-    exit()
+    sys.exit()
 
 s.listen(10)
 
-print("Socket bound and server ready.")
+print("Socket bound and server listening.")
+
+# Server function set-up
+
+def send_data(data, conn):
+    """
+    Simplify sending information to client
+    """
+    try:
+        conn.send((data +"\n").encode())
+        return True
+    except:
+        return False
+
+
+def client_connection(client):
+    """
+    Give clients a thread and a place to operate.
+    """
+
+    send_data("AVA Cloud ready.", client)
