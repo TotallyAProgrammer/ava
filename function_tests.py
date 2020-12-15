003f245d3306780826566051d26bd79e24a1c7b5
Zane Reick <zane.reick@unknownts.tk>
2020-12-15 10:01:15 -0600

    Threading test

--- function_tests.py
+++ function_tests.py
@@ -1,26 +1,15 @@
-#import ava_cmds
-#import ava_core
+import threading
+from cmd_functions import *
 
-#ava_cmds.speak("Test")
+def prnt():
+    import time
+    while True:
+        print("test")
+        time.sleep(3)
 
-#import v_programmer
-
-#v_programmer.write_verbal_command("Hello AVA", "AVA")
-
-#print(v_programmer.read_verbal_command("Hello AVAs"))
-
-# import json
-# t_phrase = "Hello AVA"
-# orig_cmd = "AVA"
-
-# dataDict = {
-#     'id': None,
-#     'trigger': str(t_phrase.lower()),
-#     'original': str(orig_cmd.lower())
-# }
-
-# dataJson = json.dumps(dataDict)
-
-# print(dataJson)
-# with open('c_v_cmds_db.json', 'w') as output:
-#     json.dump(dataJson, output)
+cloud = threading.Thread(target=cloud_sync, args=(), daemon=True)
+prnt  = threading.Thread(target=prnt, args=(), daemon=True)
+cloud.start()
+prnt.start()
+while True:
+    pass
