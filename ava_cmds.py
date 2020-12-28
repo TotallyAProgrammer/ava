a6c5a46ca27c6b206e6b257ef9be22e7cfe44e5b
Zane Reick <zane.reick@unknownts.tk>
2020-12-27 23:39:02 -0600

    Cont'd adding support for ava cloud

--- ava_cmds.py
+++ ava_cmds.py
@@ -3,6 +3,7 @@ import sys
 import speech_recognition as sr
 from v_programmer import write_verbal_command, read_verbal_command
 from cmd_functions import cloud_server_version_retrieve, increment_ava_version
+from ava_connect_clouds import connect_ava_cloud
 
 """
 AVA's primary commands file
@@ -90,46 +91,84 @@ def check_internet_connectivity():
     except requests.ConnectionError:
         return False
 
+def cloud_questions(socket, question=None):
+    """
+    Handle questions for cloud services
+    socket = the cloud socket object
+    question = the users question
+    """
+    question = str(question).lower()
+    s = socket
+
 def questions(question=None):
     """
     AVA's main questions function
     """
     # Answer Arrays
     yes_answers = {"yes", "yeah", "yep", "yup", "okay", "ok"}
+    no_answers  = {"no", "nope", "nah"}
 
     # Variables
     question = question.lower()
-    ava_version = "1"
+    ava_version = "130"
 
     # Questions
-    if question in {"ava add trigger", "hey add trigger", "eva add trigger"}: # Verbal Programmer
-        speak("Okay. What is the trigger phrase?")
-        t_phrase = take_valid_input_in()
-        # speak(t_phrase)
-        speak("Okay. What is the original command?")
-        orig_cmd = take_valid_input_in()
-        # speak(orig_cmd)
-        speak("The new trigger command is: " + t_phrase + ", for the command " + orig_cmd + ". Is this correct?")
-        answer = take_valid_input_in()
-        if answer.lower() in yes_answers:
-            speak("Writing to persistent database")
-            try:
-                write_verbal_command(t_phrase, orig_cmd)
-                speak("Write successful")
-                return True
-            except:
-                speak("Unable to write to file")
+    if cloud_mode != True:
+        if question in {"ava add trigger", "hey add trigger", "eva add trigger"}: # Verbal Programmer
+            speak("Okay. What is the trigger phrase?")
+            t_phrase = take_valid_input_in()
+            # speak(t_phrase)
+            speak("Okay. What is the original command?")
+            orig_cmd = take_valid_input_in()
+            # speak(orig_cmd)
+            speak("The new trigger command is: " + t_phrase + ", for the command " + orig_cmd + ". Is this correct?")
+            answer = take_valid_input_in()
+            if answer.lower() in yes_answers:
+                speak("Writing to persistent database")
+                try:
+                    write_verbal_command(t_phrase, orig_cmd)
+                    speak("Write successful")
+                    return True
+                except:
+                    speak("Unable to write to file")
+                    return False
+            else:
+                speak("Cancelling operation")
                 return False
-        else:
-            speak("Cancelling operation")
-            return False
-    elif question == "ava what is your version" or read_verbal_command(question) == "ava what is your version":
-        speak("My version is " + ava_version + ". My cloud server is version " + cloud_server_version_retrieve())
-    elif question == "ava exit" or read_verbal_command(question) == "ava exit":
-        speak("Okay, exitting...")
-        sys.exit()
-    elif question == "ava update version" or read_verbal_command(question) == "ava update version":
-        speak("Incrementing my version.")
-        increment_ava_version()
-        speak("Done.")
-    print(question)
+        elif question == "ava what is your version" or read_verbal_command(question) == "ava what is your version":
+            speak("My version is " + ava_version + ". My cloud server is version " + cloud_server_version_retrieve())
+        elif question == "ava exit" or read_verbal_command(question) == "ava exit":
+            speak("Okay, exitting...")
+            sys.exit()
+        elif question == "ava update version" or read_verbal_command(question) == "ava update version":
+            speak("Incrementing my version.")
+            increment_ava_version()
+            speak("Done.")
+        elif question == "ava cloud enable" or read_verbal_command(question) == "ava cloud enable":
+            speak("Switching to cloud mode...")
+            global cloud_mode = True
+        elif question == "ava is cloud mode enabled" or read_verbal_command(question) == "ava is cloud mode enabled":
+            speak("AVA Cloud mode is enabled.")
+        print(question)
+    elif cloud_mode == True:
+        if question == "ava is cloud mode enabled" or read_verbal_command(question) == "ava is cloud mode enabled":
+            speak("AVA Cloud mode is enabled.")
+        elif question == "ava exit cloud mode" or read_verbal_command(question) == "ava exit cloud mode":
+            speak("Exiting cloud mode")
+            global cloud_mode == False
+        elif question == "ava connect to ava cloud" or read_verbal_command(quesiton) == "ava connect to ava cloud":
+            speak("Attempting to connect to AVA Cloud...")
+            try:
+                global ava_c_socket = connect_ava_cloud(str(config.get('AVA_CLOUD', 'ip')), str(config.get('AVA_CLOUD', 'port')))
+                ava_cloud_connect = True
+            except Exception as exp:
+                speak("An error occured, would you like me to read it to you?")
+                while True:
+                    answer = take_valid_input_in()
+                    if answer in yes_answers:
+                        speak("Okay, here is the error.")
+                        speak(str(exp))
+                        break
+                    elif answer in no_answers:
+                        speak("Okay.")
+                        break
